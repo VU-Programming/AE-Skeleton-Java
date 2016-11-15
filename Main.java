ab07ac950efbbb2b8e7c6e146c307f2aba5ba6c6
Sebastian Österlund <s.osterlund@vu.nl>
2016-11-15 19:50:05 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static void printDummySolution() {
+        System.out.printf("15131\n");
+        System.out.printf("0 1 2 4 6 7\n");
+    }
+
+    public static final void main(String argv[]) {
+        printDummySolution();
+    }
+
+}
---
+++ prepare.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#apt-get moo
+javac Main.java
